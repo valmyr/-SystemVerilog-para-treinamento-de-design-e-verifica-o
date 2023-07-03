f521bf8c2b22a632d43e6e12a17524a5666e8e30
Valmir Ferreira da Silva <65004196+valmyr@users.noreply.github.com>
2023-07-03 19:32:25 -0300

    Update simple_multiplexor.sv

--- Lab 2 -  Modelling a Simple Multiplexor/simple_multiplexor.sv
+++ Lab 2 -  Modelling a Simple Multiplexor/simple_multiplexor.sv
@@ -5,4 +5,10 @@ module simple_multiplexor#(parameter WIDTH = 8)(
         output logic [WIDTH-1:0] out
 );
     assign out = sel_a ? in_a : in_b ;
+    always_comb begin : multiplexor
+        case(sel_a)
+            0: out = in_b;
+            1: out = in_a;
+        endcase 
+    end : multiplexor
 endmodule
